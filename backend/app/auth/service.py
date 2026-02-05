5e906c87484292f5b125bcd0ba639be901cd43d4
Elvin <elvin.mwarangu@student.moringaschool.com>
2026-02-05 13:45:15 +0300

    auth service final code

--- backend/app/auth/service.py
+++ backend/app/auth/service.py
@@ -27,3 +27,22 @@ def register_user(db: Session, name: str, email: str, password: str):
     db.refresh(user)
 
     return {"message": "User registered successfully"}
+
+
+def authenticate_user(db: Session, email: str, password: str):
+    user = db.query(User).filter(User.email == email).first()
+
+    if not user or not verify_password(password, user.password_hash):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid email or password"
+        )
+
+    token = create_access_token(
+        data={"sub": str(user.id), "role": user.role}
+    )
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
