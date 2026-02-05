bec02c94df803d12653892e4ba031258c501cbac
Elvin <elvin.mwarangu@student.moringaschool.com>
2026-02-05 13:44:20 +0300

    auth service code

--- backend/app/auth/service.py
+++ backend/app/auth/service.py
@@ -7,3 +7,23 @@ from app.core.security import (
     verify_password,
     create_access_token
 )
+def register_user(db: Session, name: str, email: str, password: str):
+    existing_user = db.query(User).filter(User.email == email).first()
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already registered"
+        )
+
+    user = User(
+        name=name,
+        email=email,
+        password_hash=hash_password(password),
+        role="CLIENT"
+    )
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return {"message": "User registered successfully"}
