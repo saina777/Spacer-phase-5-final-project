ee184fdbe51cc16928b715ef17521d2833fa9325
M.K.Thiong'o <kamauthiongo58@gmail.com>
2026-02-06 08:14:09 +0300

    React imports and App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-// import React from 'react'
-// import { Routes, Route } from 'react-router-dom'
-// import HomePage from './pages/HomePage.jsx'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx'
 
-// export default function App() {
-// 	return (
-// 		<Routes>
-// 			<Route path="/" element={<HomePage />} />
-// 		</Routes>
-// 	)
-// }
+export default function App() {
+	return (
+		<Routes>
+			<Route path="/" element={<HomePage />} />
+		</Routes>
+	)
+}
