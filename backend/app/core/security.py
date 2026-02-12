4b242ed6d557ac8cce71f26d39b27317527e6b88
Elvin <elvin.mwarangu@student.moringaschool.com>
2026-02-12 22:04:08 +0300

    fixed problem

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -3,19 +3,27 @@ from datetime import datetime, timedelta
 from jose import jwt
 from app.config import settings
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
 
-def hash_password(password: str):
-    return pwd_context.hash(password)
+# bcrypt allows max 72 bytes
+def hash_password(password: str) -> str:
+    safe_password = password[:72]
+    return pwd_context.hash(safe_password)
 
-def verify_password(password: str, hashed: str):
-    return pwd_context.verify(password, hashed)
-def create_access_token(data: dict):
+def verify_password(password: str, hashed: str) -> bool:
+    safe_password = password[:72]
+    return pwd_context.verify(safe_password, hashed)
+
+def create_access_token(data: dict) -> str:
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(
         minutes=settings.access_token_expire_minutes
     )
     to_encode.update({"exp": expire})
+
     return jwt.encode(
         to_encode,
         settings.secret_key,
